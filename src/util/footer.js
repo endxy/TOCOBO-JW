6cf88be54cb6f5fe618fcf6d124f0efe4f145fca
seyniss <shepunk2@kbu.ac.kr>
2026-03-19 21:46:46 +0900

    ins

--- src/util/footer.js
+++ src/util/footer.js
@@ -12,7 +12,13 @@ export const logoData = {
 
 // 회사 정보
 export const companyData = [
-
+  "조이웍스앤코  대표 : 송윤섭",
+  "주소 : 서울특별시 서초구 매헌로 16, 하이브랜드 패션관 5층(양재동)",
+  "사업자등록번호 : 291-88-01393  [사업자정보확인]",
+  "통신판매업신고 :  2024-서울강남-04439",
+  "TEL : 02-6352-2410  E-MAIL : help@thepicton.com",
+  "기업 문의 :  info@joyworksnco.com ",
+  "마케팅 협업 문의 : marketing@joyworksnco.com",
 ];
 export const footerLegal = {
   copyright: "COPYRIGHT© 2022 조이웍스앤코 ALL RIGHT RESERVED.",
@@ -25,8 +31,8 @@ export const footerLegal = {
 // 고객센터
 export const customerCenterData = {
   title: "Customer Center",
-  tel: { value: "070 - 4077- 5050", href: "tel:0263522410" },
-  hours: "AM 10:00 - PM 6:00 (Lunch 12:00 - 13:00)",
+  tel: { value: "02-6352-2410", href: "tel:0263522410" },
+  hours: "AM 10:00 - PM 5:00 (Lunch 12:00 - 13:00)",
   notice: "토, 일요일 및 공휴일 휴무",
   talk: { label: "1:1 TALK", href: "#" },
 };
@@ -36,33 +42,31 @@ export const footerMenus = [
   {
     title: "Brand",
     items: [
-      { label: "Brand Story", href: "/brand-story" },
-      { label: "Collection", href: "/collection" },
+      { label: "LADY", href: "/brand-story" },
+      { label: "FOR THE HOME", href: "/collection" },
+      { label: "SLOW ALLEE", href: "/collection" },
     ],
   },
   {
     title: "Shop",
     items: [
       { label: "All Product", href: "/shop" },
-      { label: "MakeUp", href: "/shop/makeup" },
-      { label: "Cleansing", href: "/shop/cleansing" },
-      { label: "Skincare", href: "/shop/skincare" },
-      { label: "Sun", href: "/shop/sun" },
-      { label: "Lip", href: "/shop/lip" },
-      { label: "Set/Etc", href: "/shop/etc" },
+      { label: "Bed", href: "/shop/makeup" },
+      { label: "Desk", href: "/shop/cleansing" },
+      { label: "Sofa", href: "/shop/skincare" },
+      { label: "Dress Room", href: "/shop/sun" },
     ],
   },
   {
-    title: "Promotion",
-    items: [{ label: "Promotion", href: "/promotion" }],
+    title: "Career",
+    items: [{ label: "Recrult", href: "/Recrult" }],
+    items: [{ label: "Location", href: "/Location" }],
   },
   {
     title: "Community",
     items: [
       { label: "Notice", href: "/notice" },
-      { label: "FAQ", href: "/faq" },
-      { label: "Review", href: "/review" },
-      { label: "1:1 CS", href: "/cs" },
+      { label: "PR/News", href: "/PR/News" },
     ],
   },
 ];
